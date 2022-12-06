3d2e02f3d42fab8b7220f63ba6929efa470ce910
yueh <y456123u@gmail.com>
2022-12-06 22:30:09 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+// const exphbs = require('express-handlebars');
+
+// app.engine('handlebars',exphbs({defaultLayout : 'main'}));
+// app.set('view engine','handlebars');
