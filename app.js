843eefda09498a98e02bef027ba893f7c5ee3b81
yueh <y456123u@gmail.com>
2022-12-08 00:01:07 +0800

    feat : add update todo function

--- app.js
+++ app.js
@@ -1,33 +1,68 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const exphbs = require('express-handlebars');
+app.use(express.urlencoded({ extended : true}));
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 const mongoose = require('mongoose');
+const Todo = require('./models/todo')
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-  }
-
+}
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })//設定連線到MONGODB
-
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
 db.on('error', () => {
-  console.log('mongodb error!')
+    console.log('mongodb error!')
 })
 // 連線成功
 db.once('open', () => {
-  console.log('mongodb connected!')
+    console.log('mongodb connected!')
 })
 
-const exphbs = require('express-handlebars');
-
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    Todo.find() // 取出 Todo model 裡的所有資料
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+        .catch(error => console.error(error)) // 錯誤處理
+})
+app.post('/todos', (req, res) => {
+    const {name} = req.body      // 從 req.body 拿出表單裡的 name 資料
+    return Todo.create({ name })     // 存入資料庫
+      .then(() => res.redirect('/')) // 新增完成後導回首頁
+      .catch(error => console.log(error))
+})
+app.get('/todos/new', (req, res) => {
+    return res.render('new')
+})
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('detail', { todo }))
+      .catch(error => console.log(error))
   })
-
-app.listen(port,()=>{
+  app.get('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+      .lean()
+      .then((todo) => res.render('edit', { todo }))
+      .catch(error => console.log(error))
+  })
+  app.post('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    return Todo.findById(id)
+      .then(todo => {
+        todo.name = name
+        return todo.save()
+      })
+      .then(()=> res.redirect(`/todos/${id}`))
+      .catch(error => console.log(error))
+  })
+app.listen(port, () => {
     console.log(`This project is now running on localhost:${port}`)
 })
