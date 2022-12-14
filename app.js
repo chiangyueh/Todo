76583db78d7baa9ca70fd4bdcdb3d643ecb6560e
yueh <y456123u@gmail.com>
2022-12-14 17:39:19 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const exphbs = require('express-handlebars');
 app.use(express.urlencoded({ extended : true}));
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+// 引用路由器
+const routes = require('./routes')
+
+
 const mongoose = require('mongoose');
-const Todo = require('./models/todo')
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
@@ -14,6 +17,8 @@ if (process.env.NODE_ENV !== 'production') {
 const methodOverride = require('method-override') 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+// 將 request 導入路由器
+app.use(routes)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })//設定連線到MONGODB
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -26,56 +31,6 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
-
-app.get('/', (req, res) => {
-    Todo.find() // 取出 Todo model 裡的所有資料
-        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-        .sort({ _id: 'asc' }) 
-        .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-        .catch(error => console.error(error)) // 錯誤處理
-})
-app.post('/todos', (req, res) => {
-    const {name} = req.body      // 從 req.body 拿出表單裡的 name 資料
-    return Todo.create({ name })     // 存入資料庫
-      .then(() => res.redirect('/')) // 新增完成後導回首頁
-      .catch(error => console.log(error))
-})
-app.get('/todos/new', (req, res) => {
-    return res.render('new')
-})
-app.get('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then((todo) => res.render('detail', { todo }))
-      .catch(error => console.log(error))
-  })
-  app.get('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .lean()
-      .then((todo) => res.render('edit', { todo }))
-      .catch(error => console.log(error))
-  })
-  app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-  app.delete('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-      .then(todo => todo.remove())
-      .then(() => res.redirect('/'))
-      .catch(error => console.log(error))
-  })
 app.listen(port, () => {
     console.log(`This project is now running on localhost:${port}`)
 })
